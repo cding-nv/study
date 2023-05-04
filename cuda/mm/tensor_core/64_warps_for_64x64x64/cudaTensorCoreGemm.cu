c712348831ca6aa45b4439913f0d412b6edf5573
Ding Feng <feng1.ding@intel.com>
2023-05-04 15:47:36 +0800

    Add tensor core 64_warps_for_64x64x64

--- cuda/mm/tensor_core/64_warps_for_64x64x64/cudaTensorCoreGemm.cu
+++ cuda/mm/tensor_core/64_warps_for_64x64x64/cudaTensorCoreGemm.cu
@@ -6,9 +6,9 @@
 #include <helper_cuda.h>
 using namespace nvcuda;
 
-#define M_GLOBAL 16
-#define K_GLOBAL 16
-#define N_GLOBAL 16
+#define M_GLOBAL 64
+#define K_GLOBAL 64
+#define N_GLOBAL 64
 
 __host__ void init_host_matrices(half *a, half *b, float *c) {
     half val = (half)(rand() % 3);
@@ -35,7 +35,7 @@ __host__ void matMultiplyOnHost(half *A, half *B, float *C, int M, int K, int N)
             float temp = 0.0;
 
             for (int k = 0; k < K; k++) {
-                temp += (float)A[i * K + k] * (float)B[k * N + j];//  (float)B[j * numBRows + k];
+                temp += (float)A[i * K + k] * (float)B[j * N + k];//  (float)B[j * numBRows + k];
             }
 
             C[i * N + j] = temp;
@@ -44,31 +44,54 @@ __host__ void matMultiplyOnHost(half *A, half *B, float *C, int M, int K, int N)
 }
 
 __global__ void wmma_ker(half *a, half *b, float *c) {
-    // https://www.anandtech.com/show/12673/titan-v-deep-learning-deep-dive/3
-    // 一个 warp 32 个 thread 完成整个 16x16x16 的矩阵乘
-    // 一个 warp 分成 8 个 groups x 4 threads， 每个 group 完成 4x8 ，共 32个 C 点，
-    // 每个 group 4 个 set （thread）， 每个 thread 负责 load 一行 即 8 个元素，
-    // 每个 thread 完成 两次 4x4x4, 得到 tile 的 4x8 的 结果
-    // 4 个 set， 得到 4 个 tile 的 4x8 的结果， 累加起来， 就是 C 的 4x8 的最终结果
-    printf("x=%d, y=%d\n",
-           blockIdx.x * blockDim.x + threadIdx.x, blockIdx.y * blockDim.y + threadIdx.y);
     // Declare the fragments
     wmma::fragment<wmma::matrix_a, 16, 16, 16, half, wmma::row_major> a_frag;
-    wmma::fragment<wmma::matrix_b, 16, 16, 16, half, wmma::row_major> b_frag;
-    wmma::fragment<wmma::accumulator, 16, 16, 16, float> c_frag;
+    wmma::fragment<wmma::matrix_b, 16, 16, 16, half, wmma::col_major> b_frag;
+    wmma::fragment<wmma::accumulator, 16, 16, 16, float> acc_frag;
 
     // Initialize the output to zero
-    wmma::fill_fragment(c_frag, 0.0f);
+    wmma::fill_fragment(acc_frag, 0.0f);
 
-    // Load the inputs
-    wmma::load_matrix_sync(a_frag, a, 16);
-    wmma::load_matrix_sync(b_frag, b, 16);
+    int lda = 64;
+    int ldb = 64;
+    int ldc = 64;
+    int warpM = (blockIdx.x * blockDim.x + threadIdx.x) / warpSize;
+    int warpN = (blockIdx.y * blockDim.y + threadIdx.y);
 
-    // Perform the matrix multiplication
-    wmma::mma_sync(c_frag, a_frag, b_frag, c_frag);
+    for (int i = 0; i < 64; i += 16) {
+        int aCol = i;
+        int aRow = warpM * 16;
 
-    // Store the output
-    wmma::store_matrix_sync(c, c_frag, 16, wmma::mem_row_major);
+        int bCol = i;
+        int bRow = warpN * 16;
+
+        // B 是 col_major 模式， 所以，  A 的列 和 B 的列 相等， 为 0， 16， 32， 48
+        // A， B 的行 也是 16 的整数倍， 用 x 表示 A 的行， y 表示 B 的行， 进行组合， 共 4x4 16种组合
+        //     即是 A ， B 矩阵 16x16 块的左上角的坐标
+        // 所以 16 种 行组合  x  4 种列组合 = 64  种组合， 即 64 个 warp 完成整个 64x64x64 的矩阵乘
+        // warp 里的每个 thread 执行的 code 是一样的   2023.5.4
+
+        // Bounds checking
+        if (aRow < 64 && aCol < 64 && bRow < 64 && bCol < 64) {
+            printf("warpM=%d, warpN=%d, x=%d, y=%d, i = %d, aRow = %d, aCol = %d, bRow = %d, bCol = %d\n", warpM, warpN,
+                   blockIdx.x * blockDim.x + threadIdx.x, blockIdx.y * blockDim.y + threadIdx.y, i, aRow, aCol, bRow, bCol);
+            // Load the inputs
+            wmma::load_matrix_sync(a_frag, a + aCol + aRow * lda, lda);
+            wmma::load_matrix_sync(b_frag, b + bCol + bRow * ldb, ldb);
+
+            // Perform the matrix multiplication
+            wmma::mma_sync(acc_frag, a_frag, b_frag, acc_frag);
+        }
+    }
+
+    // Load in the current value of c
+    int cCol = warpN * 16;  // 即 bRow
+    int cRow = warpM * 16;  // 即 ARow
+
+    // 每个 thread 完成 C 的一个点的拷贝， 这里结果 acc_flag 和 c 都是 row_major 模式，只是 B 是列模式方便计算
+    if (cRow < 64 && cCol < 64) {
+        wmma::store_matrix_sync(c + cCol + cRow * ldc, acc_frag, ldc, wmma::mem_row_major);
+    }
 }
 
 int main(int argc, char **argv) {
@@ -84,12 +107,10 @@ int main(int argc, char **argv) {
     half *A = NULL;
     half *B = NULL;
     float *C = NULL;
-    //float *D = NULL;
 
     checkCudaErrors(cudaMalloc((void**)&A, sizeof(half) * M_GLOBAL * K_GLOBAL));
     checkCudaErrors(cudaMalloc((void**)&B, sizeof(half) * N_GLOBAL * K_GLOBAL));
     checkCudaErrors(cudaMalloc((void**)&C, sizeof(float) * M_GLOBAL * N_GLOBAL));
-    //checkCudaErrors(cudaMalloc((void**)&D, sizeof(float) * M_GLOBAL * N_GLOBAL));
 
     init_host_matrices(A_h, B_h, C_h);
     matMultiplyOnHost(A_h, B_h, C_h, M_GLOBAL, K_GLOBAL, N_GLOBAL);
@@ -97,14 +118,18 @@ int main(int argc, char **argv) {
     checkCudaErrors(cudaMemcpy(A, A_h, sizeof(half) * M_GLOBAL * K_GLOBAL, cudaMemcpyHostToDevice));
     checkCudaErrors(cudaMemcpy(B, B_h, sizeof(half) * N_GLOBAL * K_GLOBAL, cudaMemcpyHostToDevice));
     checkCudaErrors(cudaMemcpy(C, C_h, sizeof(float) * M_GLOBAL * N_GLOBAL, cudaMemcpyHostToDevice));
-    //checkCudaErrors(cudaMemset(D, 0, sizeof(float) * M_GLOBAL * N_GLOBAL));
 
-    wmma_ker<<<1, 32>>>(A, B, C);
+    dim3 blockDim(32, 8);
+    //dim3 gridDim((64 + (16 * blockDim.x / 32 - 1)) / (16 * blockDim.x / 32), (64 + 16 * blockDim.y - 1) / (16 * blockDim.y));
+    dim3 gridDim(4, 1);
+
+    wmma_ker<<<gridDim, blockDim>>>(A, B, C);
     checkCudaErrors(cudaMemcpy(result_hD, C, sizeof(float)*M_GLOBAL*N_GLOBAL, cudaMemcpyDeviceToHost));
 
     for (int i = 0; i < N_GLOBAL * M_GLOBAL; i++) {
         if (fabs(result_hD[i] - C_h[i]) > 0.1f)
             printf("mismatch i=%d result_hD=%f result_host=%f\n", i, result_hD[i], C_h[i]);
+        return -1;
     }
 
     return 0;
---
+++ cuda/mm/tensor_core/64_warps_for_64x64x64/readme.txt
@@ -0,0 +1,1 @@
+4x4x4 warp completes 64x64x64 matrix multimul.
